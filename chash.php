114eac5d7a3893d829b3421c3d67bfde0e49fc97
Julio Montoya <gugli100@gmail.com>
2013-07-10 16:22:36 +0200

    Updating composer + adding doctrine migrations commands

--- chash.php
+++ chash.php
@@ -41,6 +41,14 @@ $application->addCommands(
         new \Doctrine\DBAL\Tools\Console\Command\RunSqlCommand(),
         new \Doctrine\DBAL\Tools\Console\Command\ImportCommand(),
 
+        // Migrations Commands.
+        new \Doctrine\DBAL\Migrations\Tools\Console\Command\DiffCommand(),
+        new \Doctrine\DBAL\Migrations\Tools\Console\Command\ExecuteCommand(),
+        new \Doctrine\DBAL\Migrations\Tools\Console\Command\GenerateCommand(),
+        new \Doctrine\DBAL\Migrations\Tools\Console\Command\MigrateCommand(),
+        new \Doctrine\DBAL\Migrations\Tools\Console\Command\StatusCommand(),
+        new \Doctrine\DBAL\Migrations\Tools\Console\Command\VersionCommand(),
+
         // Chash commands
         new Chash\Command\Database\RunSQLCommand(),
         new Chash\Command\Database\DumpCommand(),
