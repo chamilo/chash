acf16b4b7ce8c6c21773b29767bd622e20e5ef4b
Julio Montoya <gugli100@gmail.com>
2013-10-10 15:03:40 +0200

    No need to add the setting documents_default_visibility there's a setting that already does the job called: tool_visible_by_default_at_creation. See BT#6621

--- src/Chash/Resources/Database/1.10.0/db_main.sql
+++ src/Chash/Resources/Database/1.10.0/db_main.sql
@@ -968,8 +968,7 @@ VALUES
 ('template', NULL, 'text', 'stylesheets', 'default', 'DefaultTemplateTitle', 'DefaultTemplateComment', NULL, NULL, 1),
 ('breadcrumb_navigation_display', NULL, 'radio', 'Platform','true','BreadcrumbNavigationDisplayTitle', 'BreadcrumbNavigationDisplayComment', NULL, NULL, 1),
 ('default_calendar_view', NULL, 'radio', 'Platform','month','DefaultCalendarViewTitle', 'DefaultCalendarViewComment', NULL, NULL, 1),
-('documents_default_visibility','documents','radio','Tools','visible','DocumentDefaultVisibilityTitle','DocumentDefaultVisibilityComment',NULL, NULL, 1),
-('documents_default_visibility_defined_in_course','documents','checkbox','Tools','false','DocumentsDefaultVisibilityDefinedInCourseTitle','DocumentsDefaultVisibilityDefinedInCourseComment',NULL, NULL, 1),
+('documents_default_visibility_defined_in_course', NULL,'checkbox','Tools','false','DocumentsDefaultVisibilityDefinedInCourseTitle','DocumentsDefaultVisibilityDefinedInCourseComment',NULL, NULL, 1),
 ('chamilo_database_version', NULL, 'textfield', NULL, '1.10.0.001', 'DatabaseVersion', '', NULL, NULL, 0); -- base value, updated at end of file. Don't change here
 
 UNLOCK TABLES;
@@ -1334,8 +1333,6 @@ VALUES
 ('default_calendar_view', 'basicWeek', 'BasicWeek'),
 ('default_calendar_view', 'agendaWeek', 'Week'),
 ('default_calendar_view', 'agendaDay', 'Day'),
-('documents_default_visibility', 'visible', 'Visible'),
-('documents_default_visibility', 'invisible', 'Invisible'),
 ('documents_default_visibility_defined_in_course', 'true', 'Yes'),
 ('documents_default_visibility_defined_in_course', 'false', 'No');
 
@@ -3587,4 +3584,4 @@ CREATE TABLE curriculum_rel_user (
 
 
 -- Do not move this
-UPDATE settings_current SET selected_value = '1.10.0.048' WHERE variable = 'chamilo_database_version';
+UPDATE settings_current SET selected_value = '1.10.0.049' WHERE variable = 'chamilo_database_version';
--- src/Chash/Resources/Database/1.10.0/migrate-db-1.9.0-1.10.0-pre.sql
+++ src/Chash/Resources/Database/1.10.0/migrate-db-1.9.0-1.10.0-pre.sql
@@ -125,7 +125,6 @@ INSERT INTO settings_current (variable, subkey, type, category, selected_value, 
 INSERT INTO settings_current (variable, subkey, type, category, selected_value, title, comment, scope, subkeytext, access_url_changeable) VALUES ('default_calendar_view', NULL, 'radio', 'Platform','month','DefaultCalendarViewTitle', 'DefaultCalendarViewComment', NULL, NULL, 1);
 INSERT INTO settings_current (variable, subkey, type, category, selected_value, title, comment, scope, subkeytext, access_url_changeable) VALUES ('disable_copy_paste', NULL, 'radio', 'Platform', 'false','DisableCopyPasteTitle','DisableCopyPasteComment', NULL, NULL, 1);
 INSERT INTO settings_current (variable, subkey, type, category, selected_value, title, comment, scope, subkeytext, access_url_changeable) VALUES ('showonline','session','checkbox','Platform','true','ShowOnlineTitle','ShowOnlineComment',NULL,'ShowOnlineSession', 0);
-INSERT INTO settings_current (variable, subkey, type, category, selected_value, title, comment, scope, subkeytext, access_url_changeable) VALUES ('documents_default_visibility', NULL, 'radio','Tools','visible','DocumentDefaultVisibilityTitle','DocumentDefaultVisibilityComment',NULL, NULL, 1);
 INSERT INTO settings_current (variable, subkey, type, category, selected_value, title, comment, scope, subkeytext, access_url_changeable) VALUES ('documents_default_visibility_defined_in_course', NULL,'radio','Tools','false','DocumentsDefaultVisibilityDefinedInCourseTitle','DocumentsDefaultVisibilityDefinedInCourseComment',NULL, NULL, 1);
 
 
@@ -152,8 +151,7 @@ INSERT INTO settings_options (variable, value, display_text) VALUES ('default_ca
 INSERT INTO settings_options (variable, value, display_text) VALUES ('default_calendar_view', 'basicWeek', 'BasicWeek');
 INSERT INTO settings_options (variable, value, display_text) VALUES ('default_calendar_view', 'agendaWeek', 'Week');
 INSERT INTO settings_options (variable, value, display_text) VALUES ('default_calendar_view', 'agendaDay', 'Day');
-INSERT INTO settings_options (variable, value, display_text) VALUES ('documents_default_visibility', 'visible', 'Visible');
-INSERT INTO settings_options (variable, value, display_text) VALUES ('documents_default_visibility', 'invisible', 'Invisible');
+
 INSERT INTO settings_options (variable, value, display_text) VALUES ('documents_default_visibility_defined_in_course', 'true', 'Yes');
 INSERT INTO settings_options (variable, value, display_text) VALUES ('documents_default_visibility_defined_in_course', 'false', 'No');
 
@@ -163,4 +161,4 @@ INSERT INTO branch_transaction_status VALUES (1, 'To be executed'), (2, 'Execute
 UPDATE course_field SET field_type = 3 WHERE field_variable = 'special_course';
 
 -- Do not move this
-UPDATE settings_current SET selected_value = '1.10.0.048' WHERE variable = 'chamilo_database_version';
+UPDATE settings_current SET selected_value = '1.10.0.049' WHERE variable = 'chamilo_database_version';
