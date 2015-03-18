ccad3f9e194b36ff9676c1a9851a6c74d1e4a26a
Yannick Warnier <ywarnier@beeznest.org>
2015-03-18 00:07:47 -0500

    Minor - SQL locks added

--- src/Chash/Resources/Database/1.10.0/db_main.sql
+++ src/Chash/Resources/Database/1.10.0/db_main.sql
@@ -3482,4 +3482,6 @@ CREATE TABLE c_attendance_calendar_rel_group (
 
 
 -- Version
+LOCK TABLES settings_current WRITE;
 UPDATE settings_current SET selected_value = '1.10.0.21' WHERE variable = 'chamilo_database_version';
+UNLOCK TABLES;
