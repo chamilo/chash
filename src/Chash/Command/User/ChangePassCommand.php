a3251e98207e2bea3fd62de4a4ecbeec0d884666
Yannick Warnier <ywarnier@beeznest.org>
2019-05-06 13:04:17 -0500

    Fix user:change-pass command (remove mysql calls)

--- src/Chash/Command/User/ChangePassCommand.php
+++ src/Chash/Command/User/ChangePassCommand.php
@@ -2,7 +2,7 @@
 
 namespace Chash\Command\User;
 
-use Symfony\Component\Console\Command\Command;
+use Chash\Command\Database\CommonDatabaseCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -18,7 +18,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  * Changes a user password to the one given
  * @package Chash\Command\User
  */
-class ChangePassCommand extends CommonChamiloUserCommand
+class ChangePassCommand extends CommonDatabaseCommand
 {
     protected function configure()
     {
@@ -48,31 +48,54 @@ class ChangePassCommand extends CommonChamiloUserCommand
     {
         parent::execute($input, $output);
         $_configuration = $this->getHelper('configuration')->getConfiguration();
-        $connection = $this->getConnection($input);
+        $conn = $this->getConnection($input);
         $username = $input->getArgument('username');
         $password = $input->getArgument('password');
-        $us = "SELECT * FROM user WHERE username = '".mysql_real_escape_string($username)."'";
-        $uq = mysql_query($us);
-        $un = mysql_num_rows($uq);
-        if ($un >= 1) {
-            $enc = $_configuration['password_encryption'];
-            switch ($enc) {
-                case 'sha1':
-                    $password = sha1($password);
-                    break;
-                case 'md5':
-                    $password = md5($password);
-                    break;
-                default:
-                    $password = mysql_real_escape_string($password);
-                    break;
+        if ($conn instanceof \Doctrine\DBAL\Connection) {
+            try {
+                $us = "SELECT id FROM user WHERE username = ".$conn->quote($username);
+                $stmt = $conn->prepare($us);
+                $stmt->execute();
+                $un = $stmt->rowCount();
+            } catch (\PDOException $e) {
+                $output->write('SQL error!'.PHP_EOL);
+                throw new \RuntimeException($e->getMessage(), $e->getCode(), $e);
             }
-            $user = mysql_fetch_assoc($uq);
-            $ups = "UPDATE user SET password = '$password' WHERE user_id = ".$user['user_id'];
-            $upq = mysql_query($ups);
-            $output->writeln('User '.$username.' has new password.');
+            if ($un >= 1) {
+                $enc = $_configuration['password_encryption'];
+                $salt = sha1(uniqid(null, true));
+                switch ($enc) {
+                    case 'bcrypt':
+                        $password = $conn->quote(password_hash($password, PASSWORD_BCRYPT, ['cost' => 4, 'salt' => $salt]));
+                        break;
+                    case 'sha1':
+                        $password = $conn->quote(sha1($password));
+                        break;
+                    case 'md5':
+                        $password = $conn->quote(md5($password));
+                        break;
+                    default:
+                        $password = $conn->quote($password);
+                        break;
+                }
+                $result = $stmt->fetch(\PDO::FETCH_OBJ);
+                try {
+                    $ups = "UPDATE user
+                      SET password = $password,
+                      salt = '$salt'
+                      WHERE id = ".$result->id;
+                    $stmt = $conn->prepare($ups);
+                    $stmt->execute();
+                }catch (\PDOException $e) {
+                    $output->write('SQL error!'.PHP_EOL);
+                    throw new \RuntimeException($e->getMessage(), $e->getCode(), $e);
+                }
+                $output->writeln('User '.$username.' has new password.');
+            } else {
+                $output->writeln('Could not find user '.$username);
+            }
         } else {
-            $output->writeln('Could not find user '.$username);
+            $output->writeln('The connection does not seem to be a valid PDO connection');
         }
         return null;
     }
