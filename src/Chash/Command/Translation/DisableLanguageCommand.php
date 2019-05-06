d76ba3b466406641e9c006800211351e9619e660
Yannick Warnier <ywarnier@beeznest.org>
2019-05-06 17:49:30 -0500

    merge

--- src/Chash/Command/Translation/DisableLanguageCommand.php
+++ src/Chash/Command/Translation/DisableLanguageCommand.php
@@ -23,7 +23,7 @@ class DisableLanguageCommand extends CommonDatabaseCommand
         $this
             ->setName('translation:disable')
             ->setAliases(['tdl'])
-            ->setDescription('Disables a (enabled) language')
+            ->setDescription('Disables a language, without looking at dependencies (courses and users)')
             ->addArgument(
                 'language',
                 InputArgument::REQUIRED,
@@ -40,31 +40,40 @@ class DisableLanguageCommand extends CommonDatabaseCommand
     {
         parent::execute($input, $output);
         $_configuration = $this->getHelper('configuration')->getConfiguration();
-        $connection = $this->getConnection($input);
-        $lang = mysql_real_escape_string($input->getArgument('language'));
-        $ls = "SELECT id, english_name, available FROM language WHERE english_name = '$lang'";
-        $lq = mysql_query($ls);
-        if ($lq === false) {
-            $output->writeln('Error in query: '.mysql_error());
-            return null;
-        }
-        $num = mysql_num_rows($lq);
-        if ($num<1) {
-            $output->writeln($lang.' language not found in the database. Please make sure you use an existing language name.');
-            return null;
-        }
-        $lr = mysql_fetch_assoc($lq);
-        if ($lr['available'] == 0) {
-            $output->writeln($lang.' language is already disabled. Nothing to do.');
-            return null;
-        }
-        // Everything is OK so far, enable the language
-        $us = "UPDATE language SET available = 0 WHERE id = {$lr['id']}";
-        $uq = mysql_query($us);
-        if ($uq === false) {
-            $output->writeln('Error in query: '.mysql_error());
+        $conn = $this->getConnection($input);
+        $lang = $input->getArgument('language');
+        if ($conn instanceof \Doctrine\DBAL\Connection) {
+            $langQuoted = $conn->quote($lang);
+            $ls = "SELECT id, english_name, available FROM language WHERE english_name = $langQuoted";
+            try {
+                $stmt = $conn->prepare($ls);
+                $stmt->execute();
+            } catch (\PDOException $e) {
+                $output->write('SQL error!'.PHP_EOL);
+                throw new \RuntimeException($e->getMessage(), $e->getCode(), $e);
+            }
+            $num = $stmt->rowCount();
+            if ($num < 1) {
+                $output->writeln($lang.' language not found in the database. Please make sure you use an existing language name.');
+                return null;
+            }
+            $lr = $stmt->fetch(\PDO::FETCH_ASSOC);
+            if ($lr['available'] == 0) {
+                $output->writeln($lang.' language is already disabled. Nothing to do.');
+                return null;
+            }
+            // Everything is OK so far, enable the language
+            $us = "UPDATE language SET available = 0 WHERE id = ".$lr['id'];
+            try {
+                $uq = $stmt2 = $conn->prepare($us);
+                $stmt2->execute();
+            } catch (\PDOException $e) {
+                $output->write('SQL error!'.PHP_EOL);
+                throw new \RuntimeException($e->getMessage(), $e->getCode(), $e);
+            }
+            $output->writeln($langQuoted.' language has been disabled.');
         } else {
-            $output->writeln($lang . ' language has been disabled.');
+            $output->writeln('The connection does not seem to be a valid PDO connection');
         }
         return null;
     }
