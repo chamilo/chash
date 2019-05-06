72787b843fa74975a013e3d87bdcc7199e72f904
Yannick Warnier <ywarnier@beeznest.org>
2019-05-06 15:50:05 -0500

    merge

--- src/Chash/Command/Translation/ListLanguagesCommand.php
+++ src/Chash/Command/Translation/ListLanguagesCommand.php
@@ -28,6 +28,11 @@ class ListLanguagesCommand extends CommonDatabaseCommand
                 'availability',
                 InputArgument::OPTIONAL,
                 'Filter the availability we want (0 for disabled, 1 for enabled, empty for all).'
+            )
+            ->addArgument(
+                'count',
+                InputArgument::OPTIONAL,
+                'Show the number of users currently using each language.'
             );
     }
 
@@ -40,41 +45,65 @@ class ListLanguagesCommand extends CommonDatabaseCommand
     {
         parent::execute($input, $output);
         $_configuration = $this->getHelper('configuration')->getConfiguration();
-        $connection = $this->getConnection($input);
-        $av = mysql_real_escape_string($input->getArgument('availability'));
+        $conn = $this->getConnection($input);
+        $availability = $input->getArgument('availability');
         $current = 'english';
-        $ls = "SELECT selected_value FROM settings_current WHERE variable='platformLanguage'";
-        $lq = mysql_query($ls);
-        if ($lq === false) {
-            $output->writeln('Error in query: '.mysql_error());
-            return null;
-        } else {
-            $lr = mysql_fetch_assoc($lq);
+        $count = $input->getArgument('count');
+        if ($conn instanceof \Doctrine\DBAL\Connection) {
+            $ls = "SELECT selected_value FROM settings_current WHERE variable='platformLanguage'";
+            try {
+                $stmt = $conn->prepare($ls);
+                $stmt->execute();
+            } catch (\PDOException $e) {
+                $output->write('SQL error!'.PHP_EOL);
+                throw new \RuntimeException($e->getMessage(), $e->getCode(), $e);
+            }
+            $lr = $stmt->fetch(\PDO::FETCH_ASSOC);
             //$output->writeln('Current default language is: '.$lr['selected_value']);
             $current = $lr['selected_value'];
-        }
-        $where = '';
-        if ($av === '0') {
-            $where = 'WHERE available = 0';
-        } elseif ($av === '1') {
-            $where = 'WHERE available = 1';
-        }
-        $ls = "SELECT english_name, available FROM language ".$where." ORDER BY english_name";
-        $lq = mysql_query($ls);
-        if ($lq === false) {
-            $output->writeln('Error in query: '.mysql_error());
-            return null;
-        } else {
-            $languages = [];
-            $output->writeln("Language          | Enabled | Platform language");
-            $output->writeln("-----------------------------------------------");
-            while ($lr = mysql_fetch_assoc($lq)) {
-                $pl = '';
+            $where = '';
+            if ($availability === '0') {
+                $where = 'WHERE available = 0';
+            } elseif ($availability === '1') {
+                $where = 'WHERE available = 1';
+            }
+            $ls = "SELECT english_name, available FROM language ".$where." ORDER BY english_name";
+            try {
+                $stmt = $conn->prepare($ls);
+                $stmt->execute();
+            } catch (\PDOException $e) {
+                $output->write('SQL error!'.PHP_EOL);
+                throw new \RuntimeException($e->getMessage(), $e->getCode(), $e);
+            }
+            $titleLine = "Language          | Enabled | Platform language";
+            $titleLine2 = "-----------------------------------------------";
+            if ($count > 0) {
+                $titleLine .= " | Users";
+                $titleLine2 .= "--------";
+            }
+            $output->writeln($titleLine);
+            $output->writeln($titleLine2);
+            while ($lr = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+                $pl = ' ';
                 $l = strlen($lr['english_name']);
                 if ($lr['english_name'] == $current) {
                     $pl = '*';
                 }
-                $output->writeln($lr['english_name'].str_repeat(' ', 18-$l)."| ".$lr['available']."       | ".$pl);
+                $resultLine = $lr['english_name'].str_repeat(' ', 18 - $l)."| ".$lr['available']."       | ".$pl;
+                if ($count > 0) {
+                    $language = $conn->quote($lr['english_name']);
+                    $countUsers = "SELECT count(*) as num FROM user WHERE language = $language";
+                    try {
+                        $stmt2 = $conn->prepare($countUsers);
+                        $stmt2->execute();
+                    } catch (\PDOException $e) {
+                        $output->write('SQL error!'.PHP_EOL);
+                        throw new \RuntimeException($e->getMessage(), $e->getCode(), $e);
+                    }
+                    $countUsersNum = $stmt2->fetch(\PDO::FETCH_ASSOC);
+                    $resultLine .= "                 | ".$countUsersNum['num'];
+                }
+                $output->writeln($resultLine);
             }
         }
         return null;
