9a91c4680c49ac50146507035cb93b979cf2a7c2
Yannick Warnier <ywarnier@beeznest.org>
2019-05-06 15:15:54 -0500

    Fix user:set_language command (remove mysql calls) and allow for one user

--- src/Chash/Command/User/SetLanguageCommand.php
+++ src/Chash/Command/User/SetLanguageCommand.php
@@ -8,7 +8,7 @@
  */
 namespace Chash\Command\User;
 
-use Symfony\Component\Console\Command\Command;
+use Chash\Command\Database\CommonDatabaseCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -19,7 +19,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  * Changes the language for all platform users
  * @package Chash\Command\User
  */
-class SetLanguageCommand extends CommonChamiloUserCommand
+class SetLanguageCommand extends CommonDatabaseCommand
 {
     protected function configure()
     {
@@ -28,11 +28,16 @@ class SetLanguageCommand extends CommonChamiloUserCommand
         $this
             ->setName('user:set_language')
             ->setAliases(array('usl'))
-            ->setDescription('Sets the users language to the one given')
+            ->setDescription('Sets all the users\' language to the one given')
             ->addArgument(
                 'language',
                 InputArgument::OPTIONAL,
                 'The English name for the new language to set all users to'
+            )
+            ->addArgument(
+                'username',
+                InputArgument::OPTIONAL,
+                'The username of one user to change the language for. If not provided, changes all users'
             );
     }
 
@@ -45,42 +50,64 @@ class SetLanguageCommand extends CommonChamiloUserCommand
     {
         parent::execute($input, $output);
         $_configuration = $this->getHelper('configuration')->getConfiguration();
-        $connection = $this->getConnection($input);
-        $lang = mysql_real_escape_string($input->getArgument('language'));
-        if (empty($lang)) {
-            $ls = "SELECT DISTINCT language, count(*) as num FROM user GROUP BY 1 ORDER BY language";
-            $lq = mysql_query($ls);
-            if ($lq === false) {
-                $output->writeln('Error in query: '.mysql_error());
-                return null;
-            } else {
+        $conn = $this->getConnection($input);
+        $lang = $input->getArgument('language');
+        $username = $input->getArgument('username');
+        if ($conn instanceof \Doctrine\DBAL\Connection) {
+            if (empty($lang)) {
+                try {
+                    $ls = "SELECT DISTINCT language, count(*) as num FROM user GROUP BY 1 ORDER BY language";
+                    $stmt = $conn->prepare($ls);
+                    $stmt->execute();
+                } catch (\PDOException $e) {
+                    $output->writeln('SQL Error!'.PHP_EOL);
+                    throw new \RuntimeException($e->getMessage(), $e->getCode(), $e);
+                }
                 $output->writeln("Language\t| Number of users");
-                while ($lr = mysql_fetch_assoc($lq)) {
+                while ($lr = $stmt->fetch(\PDO::FETCH_ASSOC)) {
                     $output->writeln($lr['language']."\t\t| ".$lr['num']);
                 }
-            }
-        } else {
-            // Check available languages
-            $ls = "SELECT english_name FROM language ORDER BY english_name";
-            $lq = mysql_query($ls);
-            if ($lq === false) {
-                $output->writeln('Error in query: '.mysql_error());
-                return null;
             } else {
+                try {
+                    // Check available languages
+                    $ls = "SELECT english_name FROM language ORDER BY english_name";
+                    $stmt = $conn->prepare($ls);
+                    $stmt->execute();
+                } catch (\PDOException $e) {
+                    $output->writeln('SQL Error!'.PHP_EOL);
+                    throw new \RuntimeException($e->getMessage(), $e->getCode(), $e);
+                }
                 $languages = array();
-                while ($lr = mysql_fetch_assoc($lq)) {
+                while ($lr = $stmt->fetch(\PDO::FETCH_ASSOC)) {
                     $languages[] = $lr['english_name'];
                 }
-                if (!in_array($lang,$languages)) {
+                if (!in_array($lang, $languages)) {
                     $output->writeln($lang.' must be available on your platform before you can use it');
                     return null;
                 }
-                $lu = "UPDATE user SET language = '$lang'";
-                $lq = mysql_query($lu);
-                if ($lq === false) {
-                    $output->writeln('Error in query: '.mysql_error());
-                } else {
+                if (empty($username)) {
+                    try {
+                        $lang = $conn->quote($lang);
+                        $lu = "UPDATE user SET language = $lang";
+                        $stmt = $conn->prepare($lu);
+                        $stmt->execute();
+                    } catch (\PDOException $e) {
+                        $output->writeln('SQL Error!'.PHP_EOL);
+                        throw new \RuntimeException($e->getMessage(), $e->getCode(), $e);
+                    }
                     $output->writeln('Language set to '.$lang.' for all users');
+                } else {
+                    try {
+                        $lang = $conn->quote($lang);
+                        $username = $conn->quote($username);
+                        $lu = "UPDATE user SET language = $lang WHERE username = $username";
+                        $stmt = $conn->prepare($lu);
+                        $stmt->execute();
+                    } catch (\PDOException $e) {
+                        $output->writeln('SQL Error!'.PHP_EOL);
+                        throw new \RuntimeException($e->getMessage(), $e->getCode(), $e);
+                    }
+                    $output->writeln('Language set to '.$lang.' for user '.$username);
                 }
             }
         }
