919abd3831da6091f1458bf8cd71d623718b6023
Julio <gugli100@gmail.com>
2019-12-16 19:46:59 +0100

    Minor - restore getProjectDir

--- src/Kernel.php
+++ src/Kernel.php
@@ -33,6 +33,11 @@ class Kernel extends BaseKernel
         }
     }
 
+    public function getProjectDir(): string
+    {
+        return \dirname(__DIR__);
+    }
+
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
     {
         $container->addResource(new FileResource($this->getProjectDir().'/config/bundles.php'));
